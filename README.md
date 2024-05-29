7d29ed976b23db834b6e65cca24419b9014e7fba
Rex Whitten <rex.whitten@echo.com>
2024-05-29 13:41:24 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,115 @@
+
+# DeckApe
+
+<img src="media/logo.png" alt="Logo" width="200" height="200">
+
+
+**DeckApe** is a CLI tool for managing the local container development lifecycle workflows for locally developing containerized serverless functions.
+
+
+## Features
+
+- Build container
+- Build an image
+- Delete any local images first
+- Stop any containers using the image
+- Delete any containers using the image
+- Manage container dependencies
+
+## Installation
+
+### Prerequisites
+
+- [Node.js](https://nodejs.org/) (version 14 or higher)
+- [npm](https://www.npmjs.com/)
+
+### Steps
+
+1. Clone the repository:
+    ```bash
+    git clone https://github.com/your-username/deckape.git
+    ```
+
+2. Navigate to the project directory:
+    ```bash
+    cd deckape
+    ```
+
+3. Install dependencies:
+    ```bash
+    npm install
+    ```
+
+4. Make the script executable:
+    ```bash
+    chmod +x deckape.js
+    ```
+
+5. Install the CLI tool globally:
+    ```bash
+    npm install -g .
+    ```
+
+## Usage
+
+DeckApe provides several commands to manage your container lifecycle. Below are the available commands:
+
+### Delete Containers and Images
+
+Delete containers and images based on the container name and tag.
+
+```bash
+deckape delete <containerName> <containerTag>
+```
+
+### Rebuild and Start the Container
+
+Rebuild the Docker container and start it.
+
+```bash
+deckape rebuild <containerName> <containerTag>
+```
+
+### Publish the Container to AWS ECR
+
+Publish the Docker container to AWS Elastic Container Registry (ECR).
+
+```bash
+deckape publish <containerName> <containerTag>
+```
+
+## Examples
+
+### Delete Containers and Images
+
+```bash
+deckape delete my-container latest
+```
+
+### Rebuild and Start the Container
+
+```bash
+deckape rebuild my-container latest
+```
+
+### Publish the Container to AWS ECR
+
+```bash
+deckape publish my-container latest
+```
+
+## Contributing
+
+We welcome contributions! Please read our [contributing guidelines](CONTRIBUTING.md) for more details.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Support
+
+If you have any questions or need help, feel free to open an issue on the [GitHub repository](https://github.com/your-username/deckape/issues).
+
+## Author
+
+[Rex Whitten](https://github.com/rexwhitten)
---
+++ deckape.js
@@ -0,0 +1,106 @@
+#!/usr/bin/env node
+
+const { exec } = require('child_process');
+const { Command } = require('commander');
+const program = new Command();
+
+const runCommand = (command) => {
+    return new Promise((resolve, reject) => {
+        exec(command, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`Error executing command: ${command}\n${stderr}`);
+                reject(error);
+            } else {
+                console.log(`Command executed successfully: ${command}\n${stdout}`);
+                resolve(stdout);
+            }
+        });
+    });
+};
+
+const deleteContainer = async (containerName, containerTag) => {
+    try {
+        console.log('Killing and deleting container...');
+        await runCommand(`docker ps -aq --filter ancestor=${containerName}:${containerTag} | xargs -r docker kill`);
+        await runCommand(`docker ps -aq --filter ancestor=${containerName}:${containerTag} | xargs -r docker rm`);
+        console.log('Container killed and deleted successfully.');
+    } catch (error) {
+        console.error('Failed to kill and delete container:', error);
+    }
+};
+
+const rebuildContainer = async (containerName, containerTag) => {
+    try {
+        console.log('Stopping container...');
+        await runCommand(`docker ps -aq --filter ancestor=${containerName}:${containerTag} | xargs -r docker stop`);
+
+        console.log('Building container...');
+        await runCommand(`docker build -t ${containerName}:${containerTag} .`);
+
+        console.log('Starting container...');
+        await runCommand(`docker run --platform linux/amd64 -p 9000:8080 ${containerName}:${containerTag}`);
+
+        console.log('Container rebuilt and started successfully.');
+    } catch (error) {
+        console.error('Failed to rebuild container:', error);
+    }
+};
+
+const publishToECR = async (containerName, containerTag) => {
+    const region = 'us-east-1';
+    const accountId = "896514097725";
+    const repositoryName = containerName;
+    const ecrUri = `${accountId}.dkr.ecr.${region}.amazonaws.com/${repositoryName}`;
+
+    try {
+        console.log('Logging in to AWS ECR...');
+        await runCommand(`aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountId}.dkr.ecr.${region}.amazonaws.com`);
+
+        console.log('Creating ECR repository if it does not exist...');
+        await runCommand(`aws ecr create-repository --repository-name ${repositoryName} --region ${region} --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE || true`);
+
+        console.log('Tagging Docker image...');
+        await runCommand(`docker tag ${containerName}:${containerTag} ${ecrUri}:latest`);
+
+        console.log('Pushing Docker image to ECR...');
+        await runCommand(`docker push ${ecrUri}:latest`);
+
+        console.log('Docker image pushed to ECR successfully.');
+    } catch (error) {
+        console.error('Failed to publish Docker image to ECR:', error);
+    }
+};
+
+program
+    .name('deckape')
+    .description('CLI tool for managing local container development lifecycle workflows for locally developing containerized serverless functions.')
+    .version('1.0.0');
+
+program
+    .command('delete')
+    .description('Delete containers and images')
+    .argument('<containerName>', 'Name of the container')
+    .argument('<containerTag>', 'Tag of the container')
+    .action((containerName, containerTag) => {
+        deleteContainer(containerName, containerTag);
+    });
+
+program
+    .command('rebuild')
+    .description('Rebuild and start the container')
+    .argument('<containerName>', 'Name of the container')
+    .argument('<containerTag>', 'Tag of the container')
+    .action((containerName, containerTag) => {
+        rebuildContainer(containerName, containerTag);
+    });
+
+program
+    .command('publish')
+    .description('Publish the container to AWS ECR')
+    .argument('<containerName>', 'Name of the container')
+    .argument('<containerTag>', 'Tag of the container')
+    .action((containerName, containerTag) => {
+        publishToECR(containerName, containerTag);
+    });
+
+program.parse(process.argv);
